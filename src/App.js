7eb36f0c06f0774627e66dbe8d5204f34d55bbc8
k-mayur <mayur.kumar@mountblue.io>
2019-05-13 12:42:26 +0530

    load home page cypress test passed with expected content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Home</div>;
+  return <div className="App">Find Falcone</div>;
 }
 
 export default App;
