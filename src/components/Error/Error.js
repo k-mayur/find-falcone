a24b22eb68e74b76d84d0b05a04908568420cb24
k-mayur <mayur.kumar@mountblue.io>
2019-05-17 11:31:52 +0530

    invalid route style done

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const error = props => {
-  return <div>invalid address</div>;
+  return (
+    <div style={{ textAlign: "center", fontSize: "2em", fontWeight: "bold" }}>
+      Invalid Address : URL doesn't Exist.
+    </div>
+  );
 };
 
 export default error;
